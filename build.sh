36e9bad100c18c2d136a75359fd03335212b0d60
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2025-04-18 20:07:33 -0400

    new build script

--- build.sh
+++ build.sh
@@ -1,24 +1,25 @@
 #!/bin/zsh
 
-for f in 00-SUPERCAT.md; do
+for f in *.md; do
     gpp -o "/tmp/$f" -H $1 $f
 done
 
 pandoc -s --css=pandoc.css --metadata title=SUPERCAT \
        /tmp/00-SUPERCAT.md \
-       00-terminology.md \
-       01-bot\ turn\ start\ here.md \
-       02-general\ priorities.md \
-       03-prelude.md \
-       04-administration.md \
-       04-influence-doctrine.md \
-       05-aggression.md \
-       06-combat\ doctrine.md \
-       07-construction.md \
-       08-mobilization.md \
-       08-movement-doctrine.md \
-       96-vox-cards.md \
-       97-leaders-and-lore.md \
-       98-questions.md \
-       99-clarifications.md \
+       /tmp/00z-campaign.md \
+       /tmp/00-terminology.md \
+       /tmp/01-bot\ turn\ start\ here.md \
+       /tmp/02-general\ priorities.md \
+       /tmp/03-prelude.md \
+       /tmp/04-administration.md \
+       /tmp/04-influence-doctrine.md \
+       /tmp/05-aggression.md \
+       /tmp/06-combat\ doctrine.md \
+       /tmp/07-construction.md \
+       /tmp/08-mobilization.md \
+       /tmp/08-movement-doctrine.md \
+       /tmp/96-vox-cards.md \
+       /tmp/97-leaders-and-lore.md \
+       /tmp/98-questions.md \
+       /tmp/99-clarifications.md \
        -o output.html
