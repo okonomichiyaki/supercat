f81a8a0fc7fd2565e849a8b9ecd0e5d087973b2c
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2025-04-22 06:08:54 -0400

    merge same action same priority

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import sys
 import csv
 
+campaign = True
+
 suits = {
     'Administration': ["Tax", "Repair", "Influence"],
     'Aggression': ["Move", "Battle", "Secure"],
@@ -28,25 +30,55 @@ def indent(s, n):
         s = "\t" + s
     return s
 
+def priority_comment(p):
+    return f"<!-- priority={p} -->"
+
 def build_statement(row):
+    cond = row['Condition']
+    pref = row['Prefer']
+    action = row['Action']
+    article = row['article']
+    goal = row['Goal']
+    thing = f"{article} {goal}"
     i = 0
-    q = f"Can bot {row['Action']} {row['article']} {row['Goal']}?"
+    quest = f"Can bot {action} {thing}?"
     s = ""
-    priority = ""
-    if (row['Priority'] != "" and row['Priority'] != "99"):
-        priority = f"<!-- priority={row['Priority']} -->"
-    if (row['Condition'] != ""):
-        s = f"✦ {priority} {row['Condition']}\n\n"
-        s = s + f"- {q}\n"
+    p = row['Priority']
+    pcomment = ""
+    if p != "" and p != "99":
+        pcomment = priority_comment(p)
+    if (cond != ""):
+        s = f"✦ {pcomment} {cond}\n\n"
+        s = s + f"- {quest}\n"
     else:
-        s = f"✦ {priority} {q}\n"
-    if (row['Prefer'] != ""):
+        s = f"✦ {pcomment} {quest}\n"
+    if pref != "":
         s = s + "\n"
         s = s + indent("- Prefer:\n", i)
         i = i + 1
-        ps = [ indent(f"- {p.strip()}", i) for p in row['Prefer'].split(";")]
+        ps = [ indent(f"- {p.strip()}", i) for p in pref.split(";")]
         s = s + "\n".join(ps)
     return s
+
+def merge(rows):
+    action = rows[0]['Action']
+    pcomment = priority_comment(rows[0]['Priority'])
+    things = [f"{row['article']} {row['Goal']}" for row in rows]
+    if len(things) > 2:
+        things = ", ".join(things[0:len(things)-1]) + f", or {things[-1]}"
+    else:
+        things = f"{things[0]} or {things[1]}"
+    return f"✦ {pcomment} Can bot {action} {things}?"
+
+def mergeable(a, b):
+    return a['Priority'] == b['Priority'] and a['Action'] == b['Action'] and a['Condition'] == "" and b['Condition'] == "" and a['Prefer'] == "" and b['Prefer'] == ""
+
+def cleanup(s):
+    s = s.replace("  ", " ")
+    s = s.replace("Battle", "favorable combat")
+    for t in terms:
+        s = s.replace(t, f"<ins>{t}</ins>")
+    return s.strip()
 
 suit = sys.argv[1]
 filename = sys.argv[2]
@@ -54,14 +86,17 @@ filename = sys.argv[2]
 actions = " | ".join(suits[suit])
 print(f"# {suit} - {actions}\n\n", end="")
 
+idx = 0
+stmts = []
 with open(filename, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        if (row[suit] == '◉'):
-            stmt = build_statement(row)
-            stmt = stmt.replace("  ", " ")
-            stmt = stmt.strip()
-            stmt = f"{stmt}\n\n"
-            for term in terms:
-                stmt = stmt.replace(term, f"<ins>{term}</ins>")
-            print(stmt, end="")
+        if row[suit] == '◉' and (row['Campaign'] == "" or row['Campaign'] == "x" and campaign):
+            if idx > 0 and mergeable(stmts[idx-1][1][0], row):
+                rows = stmts[idx-1][1] + [row]
+                stmts[idx-1] = (merge(rows), rows)
+            else:
+                stmt = build_statement(row)
+                stmts = stmts + [(stmt, [row])]
+                idx = idx + 1
+    print("\n\n".join([ cleanup(s[0]) for s in stmts ]), end="")
