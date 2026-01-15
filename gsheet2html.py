3dcbfcb7d9186bab377d6ca86f7cd91c996d580f
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2026-01-14 22:20:53 -0500

    automate gsheet -> html -> cider template

--- gsheet2html.py
+++ gsheet2html.py
@@ -13,6 +13,14 @@ SUITS = [
     'Mobilization'
 ]
 
+
+def find_and_replace_in_file(filename, x, y):
+    with open(filename, 'r') as f:
+      old = f.read()
+    new = old.replace(x, y)
+    with open(filename, 'w') as f:
+      f.write(new)
+
 def get_rows(filename):
     result = []
     with open(filename, newline='') as csvfile:
@@ -28,6 +36,7 @@ def main():
 #    parser.add_argument('-c', '--campaign', action='store_true')
     parser.add_argument('-a', '--api_key')
     parser.add_argument('-u', '--url')
+    parser.add_argument('-f', '--file')
     args = parser.parse_args()
 #    campaign = args.campaign
     api_key = args.api_key
@@ -42,15 +51,22 @@ def main():
 
     all_rows = get_rows(filename)
     filtered = [ row for row in all_rows if row['Hide'] != "x" and row["Campaign"] == "" ]
+    uls = []
     for suit in SUITS:
-        print(f"<ul class=\"{suit}\">")
+        ul = [f"<ul class=\"{suit}\">"]
         rows = [ row for row in filtered if row[suit] == BULLET ]
         rows = rows[:12]
         for row in rows:
             priority = int(row["Priority"])
             shorter = row["Shorter"]
-            print(f"<li>[{priority}] {shorter}</li>")
-        print(f"</ul>")
+            ul += [f"<li>[{priority}] {shorter}</li>"]
+        ul += ["</ul>"]
+        uls += ["\n".join(ul)]
+    html = "\n".join(uls)
+    if args.file:
+        find_and_replace_in_file(args.file, "XXX", html)
+    else:
+        print(html)
 
 if __name__ == "__main__":
     main()
